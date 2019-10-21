70e1e27223b6ba9018264aadd896eecea52a39f6
Lucas <web.lucasvieira@gmail.com>
2019-10-21 15:29:15 -0300

    validação de sessão

--- gonode/go-barber-api/src/app/controllers/SessionController.js
+++ gonode/go-barber-api/src/app/controllers/SessionController.js
@@ -1,9 +1,23 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+    // Validations with Yup
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'Validation Failed',
+      });
+    }
+
+
     const { email, password } = req.body;
 
     const user = await User.findOne({
