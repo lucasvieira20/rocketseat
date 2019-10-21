64dd94ed8ae14935fbc1d19dc062d6c75653121b
Lucas <web.lucasvieira@gmail.com>
2019-10-21 19:28:37 -0300

    migration user

--- gonode/gympoint/src/database/migrations/20191021191502-create-users.js
+++ gonode/gympoint/src/database/migrations/20191021191502-create-users.js
@@ -1,7 +1,34 @@
 /* eslint-disable linebreak-style */
 module.exports = {
 	up: (queryInterface, Sequelize) => {
-		return queryInterface.createTable("users", { id: Sequelize.INTEGER });
+		return queryInterface.createTable("users", {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true
+			},
+
+			name: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				password_hash: Sequelize.STRING
+			},
+			password_hash: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+
+			created_at: {
+				type: Sequelize.DATE,
+				allowNull: false
+			},
+
+			updated_at: {
+				type: Sequelize.DATE,
+				allowNull: false
+			}
+		});
 	},
 	down: queryInterface => {
 		return queryInterface.dropTable("users");
